cd2e6fa557833993b91e8b516dc962bd09edba48
Art-Smirnov <artem@classtag.com>
2023-03-11 12:46:50 +0200

    MAR-30 add Delete All btn

--- printmall/src/components/CartProducts/CartProducts.tsx
+++ printmall/src/components/CartProducts/CartProducts.tsx
@@ -1,7 +1,7 @@
 import React, { memo, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../store/store';
-import { CartState, getTotals } from '../../store/cart/cartSlice';
+import { CartState, clearCart, getTotals } from '../../store/cart/cartSlice';
 import CartProductItem from './CartProductItem';
 import ReturnButton from '../common/Buttons/ReturnButton';
 
@@ -18,19 +18,37 @@ const CartProducts: React.FC = () => {
     dispatch(getTotals());
   }, [items, dispatch]);
 
+  const handleClearCart = (): void => {
+    dispatch(clearCart());
+  };
+
   return (
     <div className="cart-products">
       <ReturnButton />
       <h1 className="cart-products-title">Shopping Cart</h1>
-      <div className="cart-products-items">
-        {items.map((item) => (
-          <CartProductItem key={item.cart_product_id} product={item} />
-        ))}
-      </div>
-      <div className="cart-products-total">
-        Total:&nbsp;
-        <span>${cartTotalAmount}</span>
-      </div>
+      {items.length > 0 ? (
+        <>
+          <div className="cart-products-container">
+            <div className="cart-products-items">
+              {items.map((item) => (
+                <CartProductItem key={item.cart_product_id} product={item} />
+              ))}
+            </div>
+            <button
+              className="cart-products-delete-button"
+              onClick={handleClearCart}
+            >
+              Delete All
+            </button>
+          </div>
+          <div className="cart-products-total">
+            Total:&nbsp;
+            <span>${cartTotalAmount}</span>
+          </div>
+        </>
+      ) : (
+        <div>Your cart is empty.</div>
+      )}
     </div>
   );
 };
