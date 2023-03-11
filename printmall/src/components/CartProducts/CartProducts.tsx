f3c660e1ccc504bd6df35daff6b2bbbab6b4d648
Art-Smirnov <artem@classtag.com>
2023-03-11 15:34:19 +0200

    MAR-30 add localization to cart

--- printmall/src/components/CartProducts/CartProducts.tsx
+++ printmall/src/components/CartProducts/CartProducts.tsx
@@ -7,8 +7,10 @@ import ReturnButton from '../common/Buttons/ReturnButton';
 import { ReactComponent as ArrowForward } from '../images/arrow_forward.svg';
 import Button from '../common/Buttons';
 import './CartProducts.sass';
+import { useTranslation } from 'react-i18next';
 
 const CartProducts: React.FC = () => {
+  const { t } = useTranslation();
   const { items, cartTotalAmount } = useSelector<RootState, CartState>(
     (state) => state.cart
   );
@@ -26,7 +28,7 @@ const CartProducts: React.FC = () => {
   return (
     <div className="cart-products">
       <ReturnButton />
-      <h1 className="cart-products-title">Shopping Cart</h1>
+      <h1 className="cart-products-title">{t('cart.title')}</h1>
       {items.length > 0 ? (
         <>
           <div className="cart-products-container">
@@ -39,25 +41,25 @@ const CartProducts: React.FC = () => {
               className="cart-products-delete-button"
               onClick={handleClearCart}
             >
-              Delete All
+              {t('cart.deleteAllButton')}
             </button>
           </div>
           <div className="cart-products-total">
-            Subtotal:&nbsp;
+            {t('cart.totalAmount')}:&nbsp;
             <span>${cartTotalAmount}</span>
           </div>
           <Button
             className="cart-products-checkout-button"
             iconEnd={<ArrowForward />}
           >
-            Proceed to checkout
+            {t('cart.checkoutButton')}
           </Button>
           <span className="cart-products-bottom-text">
             All the taxes will be calculated while checkout
           </span>
         </>
       ) : (
-        <div>Your cart is empty.</div>
+        <div>{t('cart.emptyMessage')}</div>
       )}
     </div>
   );
