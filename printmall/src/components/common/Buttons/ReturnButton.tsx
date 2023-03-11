4e3400adcbc597fb9e127ce4761c982ef2c5c32e
Art-Smirnov <artem@classtag.com>
2023-03-11 12:55:33 +0200

    MAR-30 add localization to ReturnButton

--- printmall/src/components/common/Buttons/ReturnButton.tsx
+++ printmall/src/components/common/Buttons/ReturnButton.tsx
@@ -1,15 +1,18 @@
 import { ReactComponent as ArrowRightIcon } from '../../images/arrow_back.svg';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 const ReturnButton = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const handleReturnClick = () => {
     navigate(-1);
   };
+
   return (
     <button onClick={handleReturnClick} className="return-button">
       <ArrowRightIcon />
-      Back
+      {t('common.returnButton')}
     </button>
   );
 };
