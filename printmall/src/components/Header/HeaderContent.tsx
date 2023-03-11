0121b1dc129c0c953c2addcf9b2344ea51c941e0
Art-Smirnov <artem@classtag.com>
2023-03-11 16:34:49 +0200

    MAR-30 add mob version to cart & useScreen

--- printmall/src/components/Header/HeaderContent.tsx
+++ printmall/src/components/Header/HeaderContent.tsx
@@ -4,15 +4,24 @@ import { ReactComponent as PhoneIcon } from '../images/phone.svg';
 import HeaderButtons from './HeaderButtons';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
+import useScreen from '../hooks/useScreen';
 
 const HeaderContent = () => {
   const { t } = useTranslation();
+  const { isMobile } = useScreen();
 
   return (
     <div className="header-content">
-      <PhoneIcon />
-      <span className="header-phone">1-888-676-2660</span>
-      <span className="header-content-live-chat">{t('header.liveChat')}</span>
+      {!isMobile() && (
+        <>
+          <PhoneIcon />
+          <span className="header-phone">1-888-676-2660</span>
+          <span className="header-content-live-chat">
+            {t('header.liveChat')}
+          </span>
+        </>
+      )}
+
       <HeaderButtons />
       <LanguageSwitcher />
       {/*<Burger />*/}
