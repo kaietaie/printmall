2df0ec5f9ad2977dcb9ebd6496dfd97fd2bdd970
Ievgenii Kaieta <kaieta.ievgenii@gmail.com>
2023-03-18 11:20:47 +0200

    sku fix

--- server/product/checkout/checkout.js
+++ server/product/checkout/checkout.js
@@ -5,8 +5,8 @@ export default async function checkout (req, res) {
  // cart (sku, qty), need to find prices and calculate sub_total amount
  const skus = req.body; // "skus": [ { "sku": "1-80-5-4", "quantity": 4 },...]
  let sub_total = 0;
- console.log(skus)
- const price = await getPrice(skus.sku);
+ console.log(skus.skus[0].sku)
+ const price = await getPrice(skus.skus[0].sku);
  sub_total = price * Number(skus.quantity)
  const client_token = await paypal.generateClientToken()
  console.log(client_token)
