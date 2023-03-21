1df6c3a7d503170b5338beac10b196ed5d29649b
Art-Smirnov <artem@classtag.com>
2023-03-21 11:54:16 +0200

    MAR-51 add general-info using mock data

--- printmall/src/components/CompleteContent/GeneralInfo.tsx
+++ printmall/src/components/CompleteContent/GeneralInfo.tsx
@@ -8,30 +8,41 @@ const GeneralInfo = () => {
     selectCartTotalAmount
   );
 
+  const general_info = [
+    { title: 'Order number :', value: '51755' },
+    { title: 'Date :', value: 'November 19, 2023' },
+    { title: 'Total :', value: '344' },
+    { title: 'Payment method:', value: 'PayPal' },
+  ];
+
   //simulating adding shipping price
-  const total = subTotalAmount + 10;
+  // const total = subTotalAmount + 10;
 
   return (
     <div className="general-info">
-      <div className="general-info-item">
-        <span className="general-info-item-title">Order number :</span>
-        <span className="general-info-item-value">51755</span>
-      </div>
+      {general_info.map((item) => {
+        return (
+          <div key={item.title} className="general-info-item">
+            <span className="general-info-item-title">{item.title}</span>
+            <span className="general-info-item-value">{item.value}</span>
+          </div>
+        );
+      })}
 
-      <div className="general-info-item">
-        <span className="general-info-item-title">Date :</span>
-        <span className="general-info-item-value">3344</span>
-      </div>
+      {/*<div className="general-info-item">*/}
+      {/*  <span className="general-info-item-title">Date :</span>*/}
+      {/*  <span className="general-info-item-value">3344</span>*/}
+      {/*</div>*/}
 
-      <div className="general-info-item">
-        <span className="general-info-item-title">Total :</span>
-        <span className="general-info-item-value">{total}</span>
-      </div>
+      {/*<div className="general-info-item">*/}
+      {/*  <span className="general-info-item-title">Total :</span>*/}
+      {/*  <span className="general-info-item-value">{total}</span>*/}
+      {/*</div>*/}
 
-      <div className="general-info-item">
-        <span className="general-info-item-title">Payment method:</span>
-        <span className="general-info-item-value">PayPal</span>
-      </div>
+      {/*<div className="general-info-item">*/}
+      {/*  <span className="general-info-item-title">Payment method:</span>*/}
+      {/*  <span className="general-info-item-value">PayPal</span>*/}
+      {/*</div>*/}
     </div>
   );
 };
