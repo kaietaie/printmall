99833c395c10e787c2d8e2c386795195bc8d3d08
Ievgenii Kaieta <kaieta.ievgenii@gmail.com>
2023-03-24 00:12:32 +0200

    MAR-53 some small fix

--- server/product/checkout/captureOrder.js
+++ server/product/checkout/captureOrder.js
@@ -1,5 +1,6 @@
 import * as paypal from "./paypal-api.js";
 import saveOrder from "../../functions/saveOrder.js";
+import sendConfirmationMail from "../../mailer/sendConfirmationMail.js";
 
 // Call API with your client and get a response for your call
 export default async function capturePayPalOrder(req, res) {
