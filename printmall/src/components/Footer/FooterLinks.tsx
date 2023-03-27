617ef9e872e60f9424aee2abf211ae066d89152d
Art-Smirnov <artem@classtag.com>
2023-03-27 11:36:30 +0300

    MAR-56 add mobile version

--- printmall/src/components/Footer/FooterLinks.tsx
+++ printmall/src/components/Footer/FooterLinks.tsx
@@ -12,19 +12,17 @@ const FooterLinks = () => {
 
   return (
     <div className="footer-links">
-      <div>
-        <h3 className="footer-links-title">Pages</h3>
-        <div className="footer-links-container">
-          <div className="footer-links-column">
-            <button onClick={handleToggleModal} className="footer-links-item">
-              Contact Us
-            </button>
-            <span className="footer-links-item">Delivery Details</span>
-          </div>
-          <div className="footer-links-column">
-            <span className="footer-links-item">Terms & Conditions</span>
-            <span className="footer-links-item">Privacy Policy</span>
-          </div>
+      <h3 className="footer-links-title">Pages</h3>
+      <div className="footer-links-container">
+        <div className="footer-links-column">
+          <button onClick={handleToggleModal} className="footer-links-item">
+            Contact Us
+          </button>
+          <span className="footer-links-item">Delivery Details</span>
+        </div>
+        <div className="footer-links-column">
+          <span className="footer-links-item">Terms & Conditions</span>
+          <span className="footer-links-item">Privacy Policy</span>
         </div>
       </div>
       {isOpenModal && <SupportModal onClose={handleToggleModal} />}
