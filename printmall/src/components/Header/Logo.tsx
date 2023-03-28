083649f53127383e39efbb1df454397230dd307e
Art-Smirnov <artem@classtag.com>
2023-03-28 11:47:25 +0300

    MAR-58 add clearPaymentDetails to logo

--- printmall/src/components/Header/Logo.tsx
+++ printmall/src/components/Header/Logo.tsx
@@ -1,11 +1,23 @@
 import React, { memo } from 'react';
 import { ReactComponent as AppLogo } from '../images/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { clearPaymentDetails } from '../../store/payment/paymentSlice';
 
 const Logo: React.FC = () => {
+  const location = useLocation();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogoClick = () => {
+    if (location.pathname === '/complete') {
+      dispatch(clearPaymentDetails());
+    }
+  };
+
   return (
     <Link to="/seller/Go_A">
-      <AppLogo className="logo" />
+      <AppLogo className="logo" onClick={handleLogoClick} />
     </Link>
   );
 };
