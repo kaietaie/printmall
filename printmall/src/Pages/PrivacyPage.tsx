b136ffab225faafb2afa4555a441c0619c9b67ea
Art-Smirnov <artem@classtag.com>
2023-04-04 15:11:34 +0300

    MAR-71 add localization

--- printmall/src/Pages/PrivacyPage.tsx
+++ printmall/src/Pages/PrivacyPage.tsx
@@ -1,58 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const PrivacyPage = () => {
+  const { t } = useTranslation();
   return (
     <div className="privacy-page">
-      <h1 className="privacy-page-title">Privacy Policy</h1>
+      <h1 className="privacy-page-title">{t('privacy.title')}</h1>
 
-      <p>Updated: 04 Apr 2023</p>
+      <p>{t('privacy.updateDate')}</p>
       <p>
-        This Privacy Policy describes how Kram Market collects, uses, and shares
-        personal information of users of this website, located at&nbsp;
-        <Link to="www.kram.market">www.kram.market</Link>&nbsp; (the “Site”). By
-        using this Site, you consent to the collection, use, and disclosure of
-        your personal information as described in this Privacy Policy.
+        {t('privacy.introductionPrt1')}
+        &nbsp;<Link to="www.kram.market">www.kram.market</Link>&nbsp;
+        {t('privacy.introductionPrt2')}
       </p>
 
-      <h3>Information Collection and Use</h3>
-      <p>
-        We collect information from you when you register on our Site or place
-        an order. When ordering or registering on our Site, as appropriate, you
-        may be asked to enter your name, email address, mailing address, phone
-        number or credit card information. We will not sell, trade, or otherwise
-        transfer your personally identifiable information to outside parties.
-      </p>
+      <h3>{t('privacy.infoCollectionTitle')}</h3>
+      <p>{t('privacy.infoCollection')}</p>
 
-      <h3>Information Sharing</h3>
-      <p>
-        We do not share personal information with third parties except as
-        necessary to carry out our business or as required by law or other legal
-        processes.
-      </p>
+      <h3>{t('privacy.infoSharingTitle')}</h3>
+      <p>{t('privacy.infoSharing')}</p>
 
-      <h3>Cookies</h3>
-      <p>
-        We use cookies to help us remember and process the items in your
-        shopping cart and compile aggregate data about site traffic and site
-        interaction so that we can offer better site experiences and tools in
-        the future.
-      </p>
+      <h3>{t('privacy.cookiesTitle')}</h3>
+      <p>{t('privacy.cookies')}</p>
 
-      <h3>Security</h3>
-      <p>
-        We implement a variety of security measures to maintain the safety of
-        your personal information when you place an order or enter, submit, or
-        access your personal information.
-      </p>
+      <h3>{t('privacy.securityTitle')}</h3>
+      <p>{t('privacy.security')}</p>
 
-      <h3>Changes to this Privacy Policy</h3>
-      <p>
-        Kram Market may change this Privacy Policy from time to time. If we make
-        changes, we will notify you by revising the date at the top of the
-        policy and, in some cases, we may provide you with additional notice
-        (such as adding a statement to our homepage or sending you an email
-        notification).
-      </p>
+      <h3>{t('privacy.changePolicyTitle')}</h3>
+      <p>{t('privacy.changePolicy')}</p>
     </div>
   );
 };
