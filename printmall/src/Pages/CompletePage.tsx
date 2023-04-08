80655a11345db467d5953274970109cb30f9e4a8
Art-Smirnov <artem@classtag.com>
2023-04-08 12:12:56 +0300

    MAR-58 add redirection to seller page in CompletePage.tsx

--- printmall/src/Pages/CompletePage.tsx
+++ printmall/src/Pages/CompletePage.tsx
@@ -3,12 +3,19 @@ import CompleteContent from '../components/CompleteContent';
 import { useEffect, useRef } from 'react';
 import { clearCart } from '../store/cart/cartSlice';
 import { clearPaymentInfo } from '../store/payment/paymentSlice';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../store/store';
+import { PaymentState } from '../types/Payment';
+import { selectOrderCaptureStatus } from '../store/payment/paymentSelectors';
+import { useNavigate } from 'react-router-dom';
 
 const CompletePage = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const isFirstRender = useRef(true);
+  const status = useSelector<RootState, PaymentState['status']>(
+    selectOrderCaptureStatus
+  );
 
   useEffect(() => {
     if (isFirstRender.current) {
@@ -16,12 +23,16 @@ const CompletePage = () => {
       return;
     }
 
+    if (status !== 'succeeded') {
+      navigate(`/seller/Go_A`);
+    }
+
     dispatch(clearCart());
 
     return () => {
       dispatch(clearPaymentInfo());
     };
-  }, [dispatch]);
+  }, [dispatch, navigate, status]);
 
   return (
     <div className="complete-page">
