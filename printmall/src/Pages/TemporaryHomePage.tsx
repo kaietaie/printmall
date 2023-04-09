e974a5114026ae743199079841e3f036cd868d53
Art-Smirnov <artem@classtag.com>
2023-04-09 20:49:36 +0300

    MAR-100 add localization

--- printmall/src/Pages/TemporaryHomePage.tsx
+++ printmall/src/Pages/TemporaryHomePage.tsx
@@ -1,14 +1,14 @@
+import { memo } from 'react';
 import workOnPageImage from '../components/images/working_on_page.png';
 import ExclusiveOffers from '../components/Footer/ExclusiveOffers';
-import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const TemporaryHomePage = () => {
+  const { t } = useTranslation();
   return (
     <div className="temporary-home-page">
       <p className="temporary-home-page-message">
-        Our team is currently engaged in developing this webpage. By subscribing
-        to the news, you can be among the first to receive updates about its
-        launch.
+        {t('home.temporaryMessage')}
       </p>
       <img src={workOnPageImage} alt="Work on page" />
       <ExclusiveOffers />
