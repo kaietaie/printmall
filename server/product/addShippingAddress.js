17fb6765aa1107de79945096fd7693742cef85d3
Ievgenii Kaieta <kaieta.ievgenii@gmail.com>
2023-04-15 16:45:00 +0300

    add cart

--- server/product/addShippingAddress.js
+++ server/product/addShippingAddress.js
@@ -1,11 +1,12 @@
 import deliveryPrice from "../functions/Posta/deliveryPrice.js";
 import saveShippingInfo from "../functions/saveOrder/saveShippingInfo.js";
 
-export var shippingAddress = {}, shippingCost, shippingId; 
+export var shippingAddress = {}, shippingCost, shippingId, cart; 
 
 export default async function addShippingAddress (req, res) {
-    const { shippingInfo, cart } = req.body;
-    shippingAddress = shippingInfo
+    const { shippingInfo } = req.body;
+    shippingAddress = shippingInfo;
+    cart = req.body.cart;
     shippingId = await saveShippingInfo(shippingInfo);
     shippingCost = await deliveryPrice(cart, shippingInfo);
     return res.status(200).json(shippingCost)
